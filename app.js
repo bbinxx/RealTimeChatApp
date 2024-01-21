31174d64f7b63ab16b8b1d81e7ed71ca41b150c6
Bibin Raju <61133690+bbinxx@users.noreply.github.com>
2024-01-21 20:47:23 +0530

    public folder added for css and js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+app.use(express.static( __dirname + '/public'));
 
 //handleSocketConnections(io);
 fdata(io);
