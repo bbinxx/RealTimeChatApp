81ef31da56a8c1778cc6d828a4a08bfb01a19161
Bibin Raju <61133690+bbinxx@users.noreply.github.com>
2024-01-21 09:35:11 +0530

    Chat with SocketIO

--- controller/chatController.js
+++ controller/chatController.js
@@ -1,3 +1,68 @@
-const userService = require("./controller/user_service");
-const firestoreService = require("./controller/firestore_services");
+//socket.js
+const firestoreService = require('../models/firestore_services'); // Adjust the path if needed
+const userService = require("../models/user_service");
+  module.exports={
+    handleSocketConnections:(io)=>{
+      io.on("connection", (socket) => {
+        socket.on('disconnect', () => {
+          //console.log(socket.id + "-Disconnected");
+        });
+        
+        socket.on('msg', (data) => {
+          firestoreService.addData('chatData', { text: data });
+          io.emit('showmsg', data);
+          io.emit('showmsg', "Hello");
+         // console.log(data);  
+        });
+      });
 
+    },
+    fdata:async (io)=>{
+
+      io.on("connection", async (socket) => {
+        console.log("User connected:"+socket.id);
+        try {
+          const chatData = await firestoreService.getAllData("chatData");
+          chatData.data.forEach(chatMessage => { 
+            //console.log("getAllData");
+          // console.log(chatMessage);
+           io.emit('showmsg',chatMessage); 
+          }) ;
+          
+        } catch (error) {
+          console.error(error);
+          
+        }
+        socket.on('disconnect', () => {
+          //console.log(socket.id + "-Disconnected");
+        });
+        
+        socket.on('msg', (data) => {
+          firestoreService.addData('chatData', 
+          { 
+            uid: userService.currentUser.user['uid'],
+            time:new Date().getTime(),
+            data: data.msg,
+            userName:userService.currentUser.user.providerData[0].displayName
+             
+          }
+          );
+          io.emit('showmsg', { 
+            uid: userService.currentUser.user['uid'],
+            time:new Date().getTime(),
+            data: data.msg,
+            userName:userService.currentUser.user.providerData[0].displayName
+            
+          }
+          );
+          //console.log("addData");
+          //console.log(msgInfo);  
+        });
+      });
+
+
+
+
+
+      
+}}
